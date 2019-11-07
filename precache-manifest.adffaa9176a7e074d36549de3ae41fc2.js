ceaf05f8585d878d4e6a6feb0b1aeb7f1e46ef86
Jeff Swanner <jeffswanner93@gmail.com>
2019-11-06 23:50:18 -0500

    Updates

---
+++ precache-manifest.adffaa9176a7e074d36549de3ae41fc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ccfd76ac3e57fb726583b7bd2d6f8b2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e49e35516a4f78219438",
+    "url": "/clicky-game/static/js/2.f60726b2.chunk.js"
+  },
+  {
+    "revision": "7b67caf7d501d5862328",
+    "url": "/clicky-game/static/js/main.4432c1fe.chunk.js"
+  },
+  {
+    "revision": "95cc12df42892c33fea6",
+    "url": "/clicky-game/static/js/runtime-main.ddea1de2.js"
+  }
+]);
