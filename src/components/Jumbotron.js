0bc9ed5be180c8a3b6452d9b0d586cff2cd98fe9
Jeff Swanner <jeffswanner93@gmail.com>
2019-11-08 13:46:05 -0500

    minor tweaks

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,8 +4,8 @@ function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-            <h1 className="display-4">Meet Mr. Fin!</h1>
-            <p className="lead">Click on a picture to earn points, but don't click on any more than once!</p>
+                <h1 className="display-4">Meet Mr. Fin!</h1>
+                <p className="lead">Click on a picture to earn points, but don't click on any more than once!</p>
             </div>
         </div>
     );
